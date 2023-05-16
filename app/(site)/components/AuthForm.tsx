4d3b0cc91b4464b6b537f8acf38b8a427c85301b
raduubraduu <lefter_radu@yahoo.com>
2023-05-16 19:31:11 +0100

    created input component

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import Input from "@/app/components/inputs/Input";
 import { useCallback, useEffect, useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -10,40 +11,62 @@ const AuthForm = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const toggleVariant = useCallback(() => {
-    if (variant === 'LOGIN') {
-      setVariant('REGISTER');
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
     } else {
-      setVariant('LOGIN');
+      setVariant("LOGIN");
     }
   }, [variant]);
 
   const {
     register,
     handleSubmit,
-    formState: {
-      errors,
-    }
+    formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
-      name: '',
-      email: '',
-      password: ''
-    }
+      name: "",
+      email: "",
+      password: "",
+    },
   });
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
-    if (variant === 'REGISTER') {}
+    if (variant === "REGISTER") {
+    }
 
-    if (variant === 'LOGIN') {}
-  }
+    if (variant === "LOGIN") {
+    }
+  };
 
   const socialAction = (action: string) => {
     setIsLoading(true);
-  }
+  };
 
-  return <div>Auth Form</div>;
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div
+        className="
+  bg-white
+    px-4
+    py-8
+    shadow
+    sm:rounded-lg
+    sm:px-10
+  "
+      >
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <Input  disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+              id="name" 
+              label="Name"/>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
