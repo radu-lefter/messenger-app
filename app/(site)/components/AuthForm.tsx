1c5692bf38a20a309512aee0b95a3695b0694184
raduubraduu <lefter_radu@yahoo.com>
2023-05-18 17:49:24 +0100

    created regiter route

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,7 @@ import { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle  } from 'react-icons/bs';
+import axios from "axios";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -37,6 +38,7 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
+      axios.post('/api/register', data)
     }
 
     if (variant === "LOGIN") {
