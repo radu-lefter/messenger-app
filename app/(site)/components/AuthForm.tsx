72a544444336e4f309335385e6a54c884b2acf20
raduubraduu <lefter_radu@yahoo.com>
2023-05-19 16:17:55 +0100

    created toaster for reg error

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle  } from 'react-icons/bs';
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -39,8 +40,10 @@ const AuthForm = () => {
 
     if (variant === "REGISTER") {
       axios.post('/api/register', data)
+      .catch(() => toast.error('Something went wrong!'))
+      .finally(() => setIsLoading(false))
     }
-
+ 
     if (variant === "LOGIN") {
     }
   };
