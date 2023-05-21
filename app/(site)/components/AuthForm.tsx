1ac9868abf63603fe1b1179e2e9d89f9ea0cddb7
Radu Lefter <lefter_radu@yahoo.com>
2023-05-21 22:51:02 +0100

    created social login

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -68,6 +68,19 @@ const AuthForm = () => {
 
   const socialAction = (action: string) => {
     setIsLoading(true);
+
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error('Invalid credentials!');
+        }
+
+        if (callback?.ok) {
+          router.push('/conversations')
+        }
+      })
+      .finally(() => setIsLoading(false));
+      
   };
 
   return (
